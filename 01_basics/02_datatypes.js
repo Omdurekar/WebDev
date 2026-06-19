b5136607013094470de03903d73ffcebbeee7602
Om Durekar <durekarom@gmail.com>
2026-06-19 21:47:09 +0530

    content of 3rd vedio

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+let name = "hitesh" // string datatype 
+let age = 18 // integer datatype
+let isLoggedIn = false // boolean datatype
+let state; // undefined datatype
+let nullValue = null // null datatype
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // prints undefined
+console.log(typeof null); // prints object
