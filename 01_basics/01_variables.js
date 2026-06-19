c7006ddd0be289f6163a418a57885150ec80a9d6
Om Durekar <durekarom@gmail.com>
2026-06-19 20:45:12 +0530

    content of 2nd vedio

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123455; // cannot be changed , using semicolon is optional
+let accountName = 'om durekar' // prefer this over var coz of block scope , we can use double quotes or single quotes for string values, using semicolon is optional
+var accountBalance = 1000 // can be changed and has function scope
+accountCity = "pune" //variable without declaration, not recommended
+
+console.log(accountId) //we can print the value of accountId
+console.log([accountName, accountBalance, accountCity]) //we can print the values of accountName, accountBalance and accountCity in a tabular format
+
